1d46612823ad33e905a91d6febb5151e291def05
bandanasinghhlr-cell <bandanasingh.hlr@gmail.com>
2026-05-05 12:13:27 +0530

    data

---
+++ 2.py
@@ -0,0 +1,11 @@
+char1="hello"
+char2="java"
+#printing data type
+print(type(char1),type(char2))
+#ext=external cariable for store data
+ext=char1
+char1=char2
+char2=ext
+print(f"{char1},{char2}")
+
+ 
